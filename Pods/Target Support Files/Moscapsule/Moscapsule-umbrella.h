37e88418a50f3ad1c5a2fb4883fb4261eee9a8d2
Apple <jammer99@outlook.com>
2017-02-01 16:37:12 +0530

    images push

---
+++ Pods/Target Support Files/Moscapsule/Moscapsule-umbrella.h
@@ -0,0 +1,11 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "Moscapsule.h"
+#import "MosquittoCallbackBridge.h"
+#import "mosquitto.h"
+
+FOUNDATION_EXPORT double MoscapsuleVersionNumber;
+FOUNDATION_EXPORT const unsigned char MoscapsuleVersionString[];
+
