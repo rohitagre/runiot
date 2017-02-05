a541853d3e9b41dc409b8cb41fefe9ec5e674731
Rohit Agre <jammer99@rohits-imac.local>
2017-02-05 16:32:10 +0530

    better keyboard handling on save settings

--- runIOT/settingsViewController.swift
+++ runIOT/settingsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class settingsViewController: UIViewController, UITextFieldDelegate {
-
+    
     @IBOutlet weak var usercred: stylingText!
     @IBOutlet weak var pwd: stylingText!
     @IBOutlet weak var newscroll: UIScrollView!
@@ -26,12 +26,10 @@ class settingsViewController: UIViewController, UITextFieldDelegate {
         
         // Do any additional setup after loading the view.
     }
-  
+    
     @IBAction func saved(_ sender: UIButton) {
-        UserDefaults.standard.set(self.usercred.text, forKey: "uname")
-        UserDefaults.standard.set(self.pwd.text, forKey: "pwd")
-        self.view.endEditing(true)
-    }
+        saveCreds()
+           }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -40,18 +38,30 @@ class settingsViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldDidBeginEditing(_ textField: UITextField)
     {
-         newscroll.setContentOffset(CGPoint(x:0,y:90), animated: true)
+        newscroll.setContentOffset(CGPoint(x:0,y:90), animated: true)
     }
     
     func textFieldDidEndEditing(_ textField: UITextField)
     {
-         newscroll.setContentOffset(CGPoint(x:0,y:0), animated: true)
+        newscroll.setContentOffset(CGPoint(x:0,y:0), animated: true)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool
     {
-        textField.resignFirstResponder()
+        if(usercred == textField){
+            usercred.resignFirstResponder()
+            pwd.becomeFirstResponder()
+        }
+        else
+        {
+            textField.resignFirstResponder()
+            saveCreds()
+        }
         return true
     }
-    
+    func saveCreds() {
+        UserDefaults.standard.set(self.usercred.text, forKey: "uname")
+        UserDefaults.standard.set(self.pwd.text, forKey: "pwd")
+        self.view.endEditing(true)
+    }
 }
